7ca58b99702ac66bb369f115e5ee69f3f4ac944b
Ryan Geary <rtgnj42@gmail.com>
2018-04-11 18:22:17 -0400

    add JFrame

--- src/View.java
+++ src/View.java
@@ -1,3 +1,33 @@
-public class View {
+import javax.swing.JPanel;                                                                     
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
 
+public class View extends JPanel {
+  // define size of game
+  final static int frameWidth = 1000;
+  final static int frameHeight = 1000;
+
+  private JFrame frame;
+
+  public View() {
+	initJFrame();
+
+  }
+
+  private void initJFrame() {
+	frame = new JFrame();
+	frame.getContentPane().add(this);
+	frame.getContentPane().setBackground(Color.blue);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(frameWidth, frameHeight);
+	frame.setVisible(true);
+  }
 }
