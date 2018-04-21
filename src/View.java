3d63521355397b5a81cf38047b2150c7c40aaa23
zelinsky <zelinsky@udel.edu>
2018-04-21 00:06:42 -0400

    Revert "Added play/pause button"

--- src/View.java
+++ src/View.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class View extends JPanel {
 	// define size of game
@@ -12,39 +11,14 @@ public class View extends JPanel {
 	private final static int progressBarHeight = 40;
 
 	private int pollutionBarScalar = 2;
-
-	JButton pauseButton;
-	JPanel buttonPanel;
 	
 	View() {
-		initButton();
 		initJFrame();
 		this.setBackground(Color.cyan);
 	}
 	
-	private void initButton() {
-	  buttonPanel = new JPanel();
-	  pauseButton = new JButton("Pause");
-	  buttonPanel.add(pauseButton);
-	  buttonPanel.setBackground(Color.cyan);
-	  buttonPanel.setFocusable(false);
-	}
-
-	public void setButtonListener(ActionListener l) {
-	  pauseButton.addActionListener(l);
-	}
-
-	public void updateButton (boolean running) {
-	  if (running) {
-		pauseButton.setText("Pause");
-	  } else {
-		pauseButton.setText("Play");
-	  }
-	}
-
 	private void initJFrame() {
 		JFrame frame = new JFrame();
-		frame.getContentPane().add(buttonPanel, BorderLayout.NORTH);
 		frame.getContentPane().add(this);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(frameWidth, frameHeight);
