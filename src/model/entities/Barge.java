dc7992f257e2c948789a908d125535d9677c97dc
Ryan Geary <rtgnj42@gmail.com>
2018-05-03 15:48:52 -0400

    Added barge class; extened entity

--- src/model/entities/Barge.java
+++ src/model/entities/Barge.java
@@ -1,4 +1,33 @@
 package model.entities;
 
-public class Barge {
+import controller.requests.RequestFactory;
+import controller.requests.RequestQueue;
+import model.Model;
+import view.audio.SoundEffect;
+
+public class Barge extends Entity {
+
+    EntityType bargeType;
+    RequestQueue requestQueue;
+
+    public Barge(int x, int y, int width, int height, EntityType bargeType, RequestQueue requestQueue) {
+        super(x, y, width, height);
+        this.bargeType = bargeType;
+        this.requestQueue = requestQueue;
+    }
+
+    /**
+     * Overrides Entity's update method so that the barge doesn't sink and contribute to the world's overwhelmingly large amount of pollution.
+     */
+    public void update () {
+
+    }
+
+    /**
+     * check which type of ENtity and which type of barge this is
+     * @return either TRASH_BARGE or RECYCLING_BARGE
+     */
+    public EntityType getType() {
+        return bargeType;
+    }
 }
